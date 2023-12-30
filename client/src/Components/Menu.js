09fb672b546752096b5314fbef5f61fcfa339639
p-for-pratik <pratik9170713729@gmail.com>
2023-12-30 05:39:32 +0530

    demo2 of home page with toggle button on left side

--- client/src/Components/Menu.js
+++ client/src/Components/Menu.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 const Menu = () => {
+    const [isShowing, setIsShowing] = useState(true);
+
+    const toggleVisibility = () => {
+        setIsShowing((prevIsShowing) => !prevIsShowing);
+    };
     return (
         <div>
+            <Button onClick={toggleVisibility}>
+                {isShowing ? 'Hide Items' : 'Show Items'}
+            </Button>
 
-            <Button variant="contained" color="secondary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"55px",
-                 fontWeight: 'bold',
-            }} > Click me</Button>
-            <Button variant="contained" color="primary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"45px",
-                fontWeight: 'bold',
-            }} > Click me</Button>
-            <Button variant="contained" color="primary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"45px",
-                fontWeight: 'bold',
-            }} > Click me</Button>
-            <Button variant="contained" color="primary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"45px",
-                 fontWeight: 'bold',
-            }} > Click me</Button>
-            <Button variant="contained" color="primary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"45px",
-                fontWeight: 'bold',
-            }} > Click me</Button>
-            <Button variant="contained" color="primary" size="large" style={{
-                borderRadius: '8px', width: "100%", marginTop:"45px",
-                 fontWeight: 'bold',
-            }} > Click me</Button>
+            {isShowing && (
+                <div>
+                    <Button variant="contained" color="secondary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "55px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                    <Button variant="contained" color="primary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "45px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                    <Button variant="contained" color="primary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "45px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                    <Button variant="contained" color="primary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "45px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                    <Button variant="contained" color="primary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "45px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                    <Button variant="contained" color="primary" size="large" style={{
+                        borderRadius: '8px', width: "100%", marginTop: "45px",
+                        fontWeight: 'bold',
+                    }} > Click me</Button>
+                </div>
+            )}
+
         </div>
     );
 };
